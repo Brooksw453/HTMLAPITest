d8576fa3ed809a095ac73c59cc7106a7ffa92864
Brooks Winchell <122840510+Brooksw453@users.noreply.github.com>
2023-12-10 19:31:57 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,18 @@
-// script.js
+const promptInput = document.getElementById('prompt');
+const responseDiv = document.getElementById('response');
 
-require('dotenv').config();
-
-async function callAPI() {
-    const promptInput = document.getElementById("promptInput").value;
-    const completionWindow = document.getElementById("completionWindow");
-
-    // Retrieve your API secret key from the .env file
-    const apiKey = process.env.OPENAI_API_KEY;
-
-    try {
-        const response = await fetch('https://api.openai.com/v1/engines/davinci/completions', {
-            method: 'POST',
-            headers: {
-                'Authorization': `Bearer ${apiKey}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                prompt: promptInput,
-                max_tokens: 50, // Adjust this as needed
-            }),
-        });
-
-        const data = await response.json();
-        completionWindow.innerText = data.choices[0].text;
-    } catch (error) {
-        console.error(error);
-        completionWindow.innerText = "Error occurred while fetching data from OpenAI API.";
-    }
+function submitPrompt() {
+  const promptText = promptInput.value;
+  // Implement API call and LLM interaction logic here
+  // ...
 }
+
+// Example API call using axios
+axios.post('/api/prompt', { prompt: promptText })
+  .then(response => {
+    responseDiv.innerText = response.data.response;
+  })
+  .catch(error => {
+    console.error(error);
+  });
 
