aca372ee80d37362c5ad38a4568ab3ccfe01a4b9
Brooks Winchell <122840510+Brooksw453@users.noreply.github.com>
2023-12-10 19:41:19 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
-const promptInput = document.getElementById('prompt');
-const responseDiv = document.getElementById('response');
+const axios = require('axios');
 
-function submitPrompt() {
-  const promptText = promptInput.value;
-  // Implement API call and LLM interaction logic here
-  // ...
+const api_endpoint = 'https://chat.openai.com/';  // Chat GPT API endpoint
+const API_CHATGPT_KEY = process.env.API_CHATGPT_KEY;  // Accessing GitHub secret
+
+// Function for sending API request with prompt
+function sendPrompt(prompt) {
+  const data = {
+    prompt,
+    temperature: 0.7,
+    max_tokens: 256,
+  };
+
+  const headers = {
+    Authorization: `Bearer ${API_CHATGPT_KEY}`,
+    'Content-Type': 'application/json',
+  };
+
+  axios.post(api_endpoint, data, { headers })
+    .then(response => {
+      console.log('ChatGPT response:', response.data.choices[0].text);
+      // Handle the response and update UI with generated text
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
 }
 
-// Example API call using axios
-axios.post('/api/prompt', { prompt: promptText })
-  .then(response => {
-    responseDiv.innerText = response.data.response;
-  })
-  .catch(error => {
-    console.error(error);
-  });
+// Example usage
+const prompt = 'Generate a poem about love';
+sendPrompt(prompt);
 
